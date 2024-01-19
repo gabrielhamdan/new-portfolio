0a8e56b81bcd8b0b67abb8eeb6dc0d845a5688fc
Gabriel Hamdan <gabriel_hamdan@hotmail.com>
2024-01-19 18:09:01 -0300

    feat: removed img from project card component

--- src/components/project_card/ProjectCard.jsx
+++ src/components/project_card/ProjectCard.jsx
@@ -1,16 +1,9 @@
 import "./ProjectCard.css"
 
 export default function ProjectCard(props) {
-    const imgSrc = `/new-portfolio/src/assets/project_images/${props.imgId}.png`;
-    
     return (
         <a href={props.link} target="_blank">
             <div className="project-card-container">
-                {props.imgId > 0 &&
-
-                    <img src={imgSrc} alt="project image" className="project-card-image" />
-                }
-
                 <div className="project-card-text-container">
                     <h3>{props.title}</h3>
                     <p>{props.description}</p>
